6ddfc36daf1111f020d50be488f0bcfdce9c4e00
PBYetml <philippe.bovey@eduvaud.ch>
2022-09-20 11:15:22 +0200

    prototype et déclaration de fonctions

--- demo22/demo22.c
+++ demo22/demo22.c
@@ -4,8 +4,12 @@
 * 
 * Auteur : PBY 
 * 
-* Description : demo
+* Description : fichier pour calcul air cercle 
 * 
+* Date de création : 13.09.2022
+* Date de modification : 22.09.2022
+* 
+*
 * Remarque : aucune 
 */
 
@@ -22,28 +26,55 @@
 const double PI = 3.14;  
 const float FRACTION = 3.0 / 7.0;  // 0.429
 
+//-- prototype --// 
+float CalculerSurfaceCercle(float rayon); 
+
 
 
 //-- déclaration de fonction 
-void main()
+int main()
 {
 	//-- variable local 
 	float  surfaceCercle_m2; 
-	int8_t rayon_m = 0.0, tailleVariable;  //uint8_t int16_t uint16_t ..
-
+	int8_t rayon_m = 1, tailleVariable;  //uint8_t int16_t uint16_t ..
+	int valRetour = -1; 
 
 	//- check taille variable --//
 	tailleVariable = sizeof(int); 
 
+	//appel de fonction -> CalculerSurfaceCercle
+	surfaceCercle_m2 = CalculerSurfaceCercle(rayon_m); 
+
 	// S = r*r*p = r^2 * p 
-
 	//surfaceCercle_m2 = PI * rayon_m * rayon_m;
-	surfaceCercle_m2 = PI * pow(rayon_m, 2); 
+	//surfceCercle_m2 = PI * pow(rayon_m, 2); 
  
-
-
-
 	//-- appel de fonction
 	printf("Bonjour SLO"); 
+	printf("\n valure surfae cercle en m2 : %f ", surfaceCercle_m2); 
 
+	// return
+	return valRetour; 
+
+}
+
+
+
+
+//---
+// nom fonction :						CalculerSurfaceCercle
+// paramètre entreée / sortie / inout :		rayon / surfaceCercle
+// description : calcul de la surface d'un cercle 
+// remarque : besoin d'une constante PI + lib math pour fct pow 
+// auteur : PBY 
+// date modification : 22.09.2022
+//---
+float CalculerSurfaceCercle(float rayon)
+{
+	// déclaration variable 
+	float surface;
+
+	surface = PI * pow(rayon, 2);
+
+	return surface;
 }
